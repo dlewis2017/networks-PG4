acb4dede540fb9b21ed26d7a2aaf61ae6cc22aca
Christopher Beaufils <chrisbeaufils@Christophers-MacBook-Pro.local>
2016-11-15 22:04:27 -0500

    fixing apn

--- server/server.cpp
+++ server/server.cpp
@@ -506,7 +506,11 @@ void apn_operation(int s) {
 	fstream outputFile;
     outputFile.open(appendFile.c_str(), fstream::in | fstream::out | fstream::app);
     while (readsoFar < fileSize) {
-		if ((recvlen=recv(s,buf,sizeof(buf),0)) == -1) error("Server receiving error!\n");
+        if (fileSize - readsoFar > MAX_LINE) {
+            if ((recvlen=recv(s, buf, sizeof(buf), 0)) == -1) error("Server receiving error!\n");
+        } else {
+            if ((recvlen=recv(s, buf, filesize - read_so_far, 0)) == -1) error("Server receiving error!\n");
+        }
 		copy = string(buf, recvlen);
         outputFile << copy;
 		readsoFar += recvlen;
